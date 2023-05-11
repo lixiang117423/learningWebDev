c7635bb1e9298b84267be19ec5b02137b1f91963
lixiang <lixiang117423@gmail.com>
2023-05-11 17:18:40 +0800

    project config

--- demo01/app.py
+++ demo01/app.py
@@ -10,7 +10,17 @@ app = Flask(__name__)
 # /表示根路由
 @app.route('/')
 def hello_world():
-    return 'Hello China!'
+    return 'Hello 中国!'
+
+# 1. debug模式
+# 1.1只要开启debug模式，修改代码后不用再次运行，保存后就会重新加载，不用重启
+# 1.2 如果出现报错，浏览器会显示报错信息
+
+# 2. 修改host
+# 让其他电脑可以访问该项目
+
+# 3.修改port
+# 指定端口避免被占用等
 
 if __name__ == '__main__':
     app.run()
