1b77f0dca38a5a0949ad292a0024b75f8ecdc808
lixiang <lixiang117423@gmail.com>
2023-05-11 20:31:55 +0800

    模板对象属性访问

--- demo03/app.py
+++ demo03/app.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 定义user的类
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 # 现在是静态的模板
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("index.html") # 从模板文件夹中自动寻找文件
+    user = User(username="xiang", email="test@qq.com")
+    # 如果是字典的情况
+    person = {
+        "email":"123@qq.com",
+        "user":"lixiang"
+    }
+    return render_template("index.html", user=user, person=person) # 从模板文件夹中自动寻找文件
 
 # 动态渲染示例
 @app.route('/blog/<blog_id>')
