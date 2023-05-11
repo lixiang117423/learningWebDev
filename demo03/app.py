3e2276d308cf200085fb5c915a9ace70558f4301
lixiang <lixiang117423@gmail.com>
2023-05-11 20:51:03 +0800

    自定义过滤器

--- demo03/app.py
+++ demo03/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 # 定义user的类
 class User:
-    def __init__(self, username, email):
+    def __init__(self, username, email, value):
         self.username = username
         self.email = email
+        self.value = value
+
+# 自定义过滤器
+def minustoone(value):
+    return value-1
+app.add_template_filter(minustoone,"myfilter") # myfilter表示的是过滤器的名称
+
+def formatetime(now_time, format="%Y-%d-%m %H:%M"):
+    return now_time.strftime(format)
+app.add_template_filter(formatetime, "timefilter")
+
+
 
 # 现在是静态的模板
 @app.route('/')
@@ -25,6 +38,13 @@ def blog_detail(blog_id):
     return render_template("blog_detail.html",
                            username="小蓝哥",
                            blog_id=blog_id) # 第一个blog_id是传入前段html文件的，第二个是后端的这个blog_id
+
+# 过滤器使用
+@app.route('/filter')
+def filter_demo():
+    user = User(username="xiang123495", email="test@qq.com", value=100)
+    time = datetime.now()
+    return render_template("filter.html", user=user, time=time)
 
 if __name__ == '__main__':
     app.run()
